0ed09b7247a72a3905c26d5eb0a4f44f048b2266
Hugo Alliaume <kocal@live.fr>
2017-11-11 21:51:43 +0100

    Add Solary's gradient as popup background

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -6,11 +6,33 @@
 
 <script>
   export default {
-    data () {
+    data() {
       return {}
     }
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+  html {
+    font-size: 62.5%;
+
+    background: #1196ff;
+    background: -moz-linear-gradient(45deg, #1196ff 0, #4940ff 25%, #d4886b 75%, #ffc919 100%);
+    background: -webkit-linear-gradient(45deg, #1196ff 0, #4940ff 25%, #d4886b 75%, #ffc919 100%);
+    background: linear-gradient(45deg, #1196ff 0, #4940ff 25%, #d4886b 75%, #ffc919 100%);
+  }
+
+  body {
+    $gutter: 4px;
+
+    height: calc(580px - 2 * #{$gutter});
+    min-width: calc(780px - 2 * #{$gutter});
+
+    margin: $gutter;
+    padding: 2 * $gutter;
+
+    font-size: 1.6rem;
+
+    color: lighten(#222, 60%);
+  }
 </style>
