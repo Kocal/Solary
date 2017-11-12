6bcdb442d6a77b03fc876b09ec6c3697ec3b2827
Hugo Alliaume <kocal@live.fr>
2017-11-12 23:17:53 +0100

    Add channels & clientIds in Store

---
+++ src/store/channels.js
@@ -0,0 +1,53 @@
+import Channel from '../entities/Channel';
+
+export default [
+  new Channel('solary', 'Solary', {
+    twitter: 'SolaryTV',
+    facebook: 'SolaryTV',
+    youtube: 'channel/UCb3c6rB0Ru1i9EUcc-a5ZJw',
+  }),
+
+  new Channel('caelaneuw', 'Caelan', {
+    twitter: 'CaelanEuw',
+    facebook: 'CaelanEuw',
+    youtube: 'channel/UCum_JcCzsOGURxknkrlcObw',
+  }),
+
+  new Channel('chap_gg', 'Chap', {
+    twitter: 'Chap_gg',
+    facebook: 'ChapGG',
+    youtube: 'channel/UCQHwykOqDhJbsGz8S5epMIg',
+  }),
+
+  new Channel('jbzzed', 'Jbzz', {
+    twitter: 'jbzzed',
+    facebook: 'theJbzz',
+    youtube: 'channel/UCJ99r-lqOr0rqsg0MjetNzA',
+  }),
+
+  new Channel('wpkatare', 'Katare', {
+    twitter: 'KatareLOL',
+  }),
+
+  new Channel('leroibisou', 'Le Roi Bisou', {
+    twitter: 'LRB_LoL',
+    facebook: 'LeRoiBisou',
+    youtube: 'user/lrbetsarens',
+  }),
+
+  new Channel('narkuss_lol', 'Narkuss', {
+    twitter: 'NarkussLol',
+    facebook: 'Narkuss',
+    youtube: 'Narkuss',
+  }),
+
+  new Channel('tiotio1993', 'Tio', {
+    twitter: 'TioooLol'
+  }),
+
+  new Channel('wakzlol', 'Wakz', {
+    twitter: 'Wakzlol',
+    facebook: 'wakzlol',
+    youtube: 'channel/UCNLTFDLglK_OO1b-mzY0gWQ',
+  }),
+]
