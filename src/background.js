b398da303ca4d78e87776ec0547828571ec8a46a
Hugo Alliaume <kocal@live.fr>
2017-11-15 21:26:41 +0100

    Add GamesManager for making easier to game name by it's id

--- src/background.js
+++ src/background.js
@@ -1,8 +1,11 @@
 import ClientIdsManager from './services/ClientIdsManager';
 import ChannelsManager from './services/ChannelsManager';
+import GamesManager from './services/GamesManager';
+
 import clientIds from './store/clientIds';
 import channels from './store/channels';
 
 const clientIdsManager = new ClientIdsManager(clientIds);
 const channelsManager = new ChannelsManager(channels, clientIdsManager);
+const gamesManager = new GamesManager(clientIdsManager);
 
