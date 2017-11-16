9022ed63b6b5bcffaa3895d8421241070f30ea2b
Hugo Alliaume <kocal@live.fr>
2017-11-16 22:29:15 +0100

    Popup: visual changes in Channel, also <a> is now the root of the component

--- src/popup/components/Channel.vue
+++ src/popup/components/Channel.vue
@@ -1,29 +1,26 @@
 <template>
-  <div class="channel" :class="[size]">
-    <a :href="channel.url()" class="channel__link" target="_blank">
-      <div v-if="channel.online" class="grid">
-        <div class="grid__aside">
-          <thumbnail :url="channel.stream.thumbnail_url" style="display: block"></thumbnail>
-        </div>
-        <div class="grid__main">
-          <h2 class="channel__nickname">
-            <a :href="channel.url()" target="_blank">{{ channel.nickname }}</a>
-          </h2>
-          <p class="channel__stream-title">{{ channel.stream.title }}</p>
-          <span class="channel__stream-status">
+  <a :href="channel.url()" class="channel" :class="[size, {offline: !channel.online}]" target="_blank">
+    <div v-if="channel.online" class="grid">
+      <div v-if="size === 'big'" class="grid__aside">
+        <thumbnail :url="channel.stream.thumbnail_url" style="display: block"></thumbnail>
+      </div>
+      <div class="grid__main">
+        <h2 class="channel__nickname">
+          <a :href="channel.url()" target="_blank">{{ channel.nickname }}</a>
+        </h2>
+        <p class="channel__stream-title">{{ channel.stream.title }}</p>
+        <span class="channel__stream-status">
             Joue à
-            <a :href="'https://www.twitch.tv/directory/game/' + channel.stream.game"
-               target="_blank">{{ channel.stream.game }}</a>
+            <a :href="'https://www.twitch.tv/directory/game/' + channel.stream.game" target="_blank">{{ channel.stream.game }}</a>
             devant <em>{{ channel.stream.viewers }}</em> viewers
           </span>
-        </div>
       </div>
-      <template v-else>
-        <h2 class="channel__nickname">{{ channel.nickname }}</h2>
-        <span class="channel__state--offline">Actuellement hors-ligne</span>
-      </template>
-    </a>
-  </div>
+    </div>
+    <template v-else>
+      <h2 class="channel__nickname">{{ channel.nickname }}</h2>
+      <span class="channel__state--offline">Actuellement hors-ligne</span>
+    </template>
+  </a>
 </template>
 
 <script>
@@ -48,30 +45,38 @@
 
 <style scoped lang="scss">
   .channel {
-    background-color: #1b1c1d;
-    box-shadow: 0 0 1px #000;
-  }
-
-  .channel__link {
     display: block;
     padding: 8px;
+
+    background-color: #1b1c1d;
+    box-shadow: 0 0 1px #000;
 
     transition: background-color .1s linear;
 
     &:hover {
-      background-color: rgba(#fff, .05);
+      background-color: rgba(#1b1c1d, .90);
+    }
+
+    &.offline {
+      filter: grayscale(1);
+    }
+
+    &.big {
+      .channel__nickname {
+        font-size: 3.2rem;
+      }
     }
   }
 
   .channel__nickname {
-    margin: .5em 0;
+    margin: .25em 0;
     font-size: 2rem;
     font-weight: 400;
     color: #ffcc00;
   }
 
-  .big .channel__nickname {
-    font-size: 3.2rem;
+  .channel__stream-title {
+    margin: .5em 0;
   }
 
   .channel__stream-status {
@@ -96,10 +101,6 @@
 
   .grid {
     display: flex;
-  }
-
-  .grid__aside {
-
   }
 
   .grid__main {
