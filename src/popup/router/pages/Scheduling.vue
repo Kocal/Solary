a100e14477bac7109a226dba564b7ea071a2aad1
Hugo Alliaume <kocal@live.fr>
2017-11-16 23:48:58 +0100

    Popup: scheduling is now displayed

--- src/popup/router/pages/Scheduling.vue
+++ src/popup/router/pages/Scheduling.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
-    La programmation
+    <a v-if="imageUrl" :href="pageUrl" target="_blank">
+      <s-image :url="imageUrl"></s-image>
+    </a>
+    <p v-if="error">Une erreur s'est produite lors de la récupération de la programmation.</p>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+  import Image from '../../components/Image.vue';
+
   export default {
-    data () {
-      return {}
+    components: {
+      's-image': Image,
+    },
+    data() {
+      return {
+        pageUrl: 'https://www.solary.fr/programme/',
+        imageUrl: null,
+        error: null,
+      }
+    },
+    created() {
+      axios.get(this.pageUrl)
+        .then(response => response.data)
+        .then(html => {
+          console.log(html);
+          const [_, imageUrl] = html.match(/<div class="prog-bg" style="background-image: url\('([^']+)'\);">/);
+
+          this.imageUrl = `https://www.solary.fr/${imageUrl}`
+        })
+        .catch(error => {
+          this.error = true;
+          console.error(error);
+        })
     }
   }
 </script>
+
+<style scoped>
+  img {
+    max-width: 100%;
+    display: block;
+  }
+</style>
