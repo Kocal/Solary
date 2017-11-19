35de170b8394d05c54a28a41c4f6ffc150cc1a8c
Hugo Alliaume <kocal@live.fr>
2017-11-19 22:41:53 +0100

    Remove useless code in Streams.vue

--- src/popup/router/pages/Streams.vue
+++ src/popup/router/pages/Streams.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <channel :channel="solaryChannel" size="big"></channel>
-    <div class="grid">
-      <template v-for="channel in allChannelsExceptSolary">
-        <channel :channel="channel"></channel>
-      </template>
-    </div>
   </div>
 </template>
 
@@ -20,18 +15,6 @@
       solaryChannel() {
         return this.$root.channels.find(channel => channel.nickname === 'Solary');
       },
-      allChannelsExceptSolary() {
-        return this.$root.channels.filter(channel => channel.nickname !== 'Solary');
-      },
     },
   };
 </script>
-
-<style scoped>
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 1px;
-    margin-top: 1px;
-  }
-</style>
