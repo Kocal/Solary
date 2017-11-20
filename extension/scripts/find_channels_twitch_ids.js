4f35a84c93aacdd2545ef368bcd9cad674706927
Hugo Alliaume <kocal@live.fr>
2017-11-20 21:15:09 +0100

    Run ESLint fix on whole project

--- extension/scripts/find_channels_twitch_ids.js
+++ extension/scripts/find_channels_twitch_ids.js
@@ -5,14 +5,14 @@ import channels from './../src/store/channels';
 import clientIds from './../src/store/clientIds';
 
 const headers = {
-  'Accept': 'application/vnd.twitchtv.v5+json',
-  'Client-ID': clientIds[0]
+  Accept: 'application/vnd.twitchtv.v5+json',
+  'Client-ID': clientIds[0],
 };
 
-axios.get('https://api.twitch.tv/kraken/users/?login=' + channels.map(c => c.username).join(','), {headers})
+axios.get(`https://api.twitch.tv/kraken/users/?login=${channels.map(c => c.username).join(',')}`, { headers })
   .then(response => response.data)
-  .then(data => {
-    data.users.forEach(user => {
+  .then((data) => {
+    data.users.forEach((user) => {
       console.log(user._id, user.name);
     });
   })
