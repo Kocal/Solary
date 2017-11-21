ecba326f8f2552eb4adb7acf675f1796ee4d8124
Hugo Alliaume <kocal@live.fr>
2017-11-21 07:38:54 +0100

    Server: created https and websocket servers

--- server/index.js
+++ server/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const fs = require('fs');
 const auth = require('http-auth');
+const https = require('https');
+const WebSocketServer = require('uws').Server;
 
+const config = require('./config.js');
 const port = process.env.PORT || 3000;
+
+// Express
+const app = express();
+app.set('view engine', 'ejs');
+
+const httpsServer = https.createServer({
+  cert: fs.readFileSync(config.cert),
+  key: fs.readFileSync(config.key),
+}, app);
+
+// WebSocket
+const wss = new WebSocketServer({
+  server: httpsServer,
+  path: '/',
+});
+
+wss.on('connection', (ws) => {
+  ws.send('Connected to WebSocket server.');
+});
+
+// Auth
 const basic = auth.basic({
   realm: 'Authentification nécessaire',
   file: `${__dirname}/.htpasswd`,
 });
 
-const app = express();
-app.set('view engine', 'ejs');
-
+// App
 app.get('/', auth.connect(basic), (req, res) => {
   res.render('home', {
     user: req.user,
@@ -21,6 +44,6 @@ app.get('/logout', auth.connect(basic), (req, res) => {
   res.end('Disconnected.');
 });
 
-app.listen(port, () => {
+httpsServer.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
