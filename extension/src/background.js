eb55fdbf5a782b9406eddcab60b6572effae7752
Hugo Alliaume <kocal@live.fr>
2017-11-23 22:42:52 +0100

    Extension: create WebSocketManager

--- extension/src/background.js
+++ extension/src/background.js
@@ -4,7 +4,9 @@ import GamesManager from './services/GamesManager';
 import NotificationsManager from './services/NotificationsManager';
 import SchedulingManager from './services/SchedulingManager';
 import BrowserActionManager from './services/BrowserActionManager';
+import WebSocketManager from './services/WebSocketManager';
 
+import webSocketConfig from './config/webSocket';
 import clientIds from './store/clientIds';
 import channels from './store/channels';
 
@@ -14,6 +16,7 @@ const notificationsManager = new NotificationsManager(channels);
 const browserActionManager = new BrowserActionManager(channels);
 const channelsManager = new ChannelsManager(channels, clientIdsManager, gamesManager, notificationsManager, browserActionManager);
 const schedulingManager = new SchedulingManager();
+const webSocketManager = new WebSocketManager(webSocketConfig, notificationsManager);
 
 channelsManager.requestTwitchApi();
 channelsManager.enableAutoRequestTwitchApi();
