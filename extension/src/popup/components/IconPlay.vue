7f2e6da70ce82583dfc37d1122e6cbbcc9309606
Hugo Alliaume <kocal@live.fr>
2017-11-28 22:39:36 +0100

    Popup: add color transition on IconPlay

--- extension/src/popup/components/IconPlay.vue
+++ extension/src/popup/components/IconPlay.vue
@@ -5,6 +5,19 @@
       width="100"
       height="100"
   >
-    <path d="M 95,50 5,95 5,5 z" fill="#1196ff"/>
+    <path d="M 95,50 5,95 5,5 z"/>
   </svg>
 </template>
+
+<style scoped lang="scss">
+  $color: #1196ff;
+
+  svg path {
+    fill: $color;
+    transition: fill .5s;
+  }
+
+  svg:hover path {
+    fill: lighten($color, 10%);
+  }
+</style>
