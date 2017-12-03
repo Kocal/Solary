a5e8004b78a108c1cffbcd3810a44b1c9acecb9a
Hugo Alliaume <kocal@live.fr>
2017-12-03 23:13:00 +0100

    CI: add scripts for server

---
+++ ci/stop-server.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+cd "../packages/server"
+
+yarn stop
---
+++ ci/test-server.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+# Check pm2
+npx pm2 log --nostream
+npx pm2 info solary
+test $? = 0
+
+# Check connection
+RESPONSE=$(curl https://localhost:3000 --insecure)
+test $? = 0;
+test "${RESPONSE}" = "401 Unauthorized" # .htpasswd
+
+# Check websocket
+set +e # timeout command will exit with status != 124, so it will stop script execution
+WEBSOCKET_RESPONSE=$(NODE_TLS_REJECT_UNAUTHORIZED=0 timeout 1s wsc -er wss://localhost:3000)
+sleep 2
+test $? = 124 # timeout
+set -e # re-enable error control
+
+echo ${WEBSOCKET_RESPONSE} | grep "Connected to WebSocket server."
+test $? = 0
