b576f4932d005045f5c60ff783791ab6b50d6892
Hugo Alliaume <kocal@live.fr>
2018-01-05 13:23:30 +0100

    Fix tests due to last changes

--- packages/extension/__tests__/services/BrowserActionManager.test.ts
+++ packages/extension/__tests__/services/BrowserActionManager.test.ts
@@ -1,16 +1,13 @@
 import channels from '../../src/store/channels';
 import { BrowserActionManager } from '../../src/services/BrowserActionManager';
-import * as chromeBrowserAction from '../../src/utils/ChromeBrowserAction';
 import Stream from '../../src/entities/Stream';
-
-jest.mock('../../src/utils/ChromeBrowserAction');
 
 describe('Service - BrowserActionManager', () => {
   describe('constructor()', () => {
     new BrowserActionManager(channels);
 
     it('should set browser action in loading state', () => {
-      expect(chromeBrowserAction.setBadgeText).toHaveBeenCalledWith('...');
+      expect(chrome.browserAction.setBadgeText).toHaveBeenCalledWith({ text: '...' });
     });
   });
 
@@ -18,18 +15,18 @@ describe('Service - BrowserActionManager', () => {
     const browserActionManager = new BrowserActionManager(channels);
 
     beforeEach(() => {
-      chromeBrowserAction.setBadgeText.mockReset();
-      chromeBrowserAction.setBadgeColor.mockReset();
-      chromeBrowserAction.setTitle.mockReset();
+      chrome.browserAction.setBadgeText.mockReset();
+      chrome.browserAction.setBadgeBackgroundColor.mockReset();
+      chrome.browserAction.setTitle.mockReset();
     });
 
     it('should set browser action in offline state', () => {
       channels[0].markAsOffline();
       browserActionManager.update();
 
-      expect(chromeBrowserAction.setBadgeText).toHaveBeenCalledWith('OFF');
-      expect(chromeBrowserAction.setBadgeColor).toHaveBeenCalledWith('gray');
-      expect(chromeBrowserAction.setTitle).toHaveBeenCalledWith('Personne ne stream actuellement sur la TV !');
+      expect(chrome.browserAction.setBadgeText).toHaveBeenCalledWith({ text: 'OFF' });
+      expect(chrome.browserAction.setBadgeBackgroundColor).toHaveBeenCalledWith({ color: 'gray' });
+      expect(chrome.browserAction.setTitle).toHaveBeenCalledWith({ title: 'Personne ne stream actuellement sur la TV !' });
     });
 
     it('should set browser action in online state', () => {
@@ -37,9 +34,9 @@ describe('Service - BrowserActionManager', () => {
 
       browserActionManager.update();
 
-      expect(chromeBrowserAction.setBadgeText).toHaveBeenCalledWith('ON');
-      expect(chromeBrowserAction.setBadgeColor).toHaveBeenCalledWith('green');
-      expect(chromeBrowserAction.setTitle).toHaveBeenCalledWith(`Midi Chapi\n\nJoue à LoL devant 6000 viewers`);
+      expect(chrome.browserAction.setBadgeText).toHaveBeenCalledWith({ text: 'ON' });
+      expect(chrome.browserAction.setBadgeBackgroundColor).toHaveBeenCalledWith({ color: 'green' });
+      expect(chrome.browserAction.setTitle).toHaveBeenCalledWith({ title: `Midi Chapi\n\nJoue à LoL devant 6000 viewers` });
     });
 
     it('should not touch browser action state if no Solary streams found (should not happens)', () => {
@@ -47,9 +44,9 @@ describe('Service - BrowserActionManager', () => {
 
       browserActionManager.update();
 
-      expect(chromeBrowserAction.setBadgeText).not.toHaveBeenCalled();
-      expect(chromeBrowserAction.setBadgeColor).not.toHaveBeenCalled();
-      expect(chromeBrowserAction.setTitle).not.toHaveBeenCalled();
+      expect(chrome.browserAction.setBadgeText).not.toHaveBeenCalled();
+      expect(chrome.browserAction.setBadgeBackgroundColor).not.toHaveBeenCalled();
+      expect(chrome.browserAction.setTitle).not.toHaveBeenCalled();
     })
   });
 });
