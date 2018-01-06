653527d2a995d5cab1d13579953708b51d948a31
Hugo Alliaume <kocal@live.fr>
2018-01-06 10:30:58 +0100

    NotificationsManager: add tests

--- packages/extension/src/services/NotificationsManager.ts
+++ packages/extension/src/services/NotificationsManager.ts
@@ -1,5 +1,14 @@
 import Channel from '../entities/Channel';
 
+const create = (title: string, message: string, id: string = '') => {
+  chrome.notifications.create(id, {
+    type: 'basic',
+    iconUrl: '../icons/icon_128.png',
+    title: title || '',
+    message: message || '',
+  });
+};
+
 class NotificationsManager {
   constructor(private channels: Array<Channel>) {
     this.setupNotificationClickHandler();
@@ -7,24 +16,14 @@ class NotificationsManager {
 
   public show(channel: Channel): void {
     if (!channel.stream) {
-      return console.error(`Le channel ${channel.username} n'est pas en ligne, impossible d'afficher une notification.`);
+      return console.error(`Le channel ${channel.nickname} n'est pas en ligne, impossible d'afficher une notification.`);
     }
 
-    chrome.notifications.create(channel.username, {
-      type: 'basic',
-      iconUrl: '../icons/icon_128.png',
-      title: `${channel.nickname} est en live sur ${channel.stream.game} !`,
-      message: channel.stream.title || ' ',
-    });
+    create(`${channel.nickname} est en live sur ${channel.stream.game} !`, channel.stream.title, channel.username);
   }
 
   public showByTitleAndMessage(title: string, message: string): void {
-    chrome.notifications.create({
-      type: 'basic',
-      iconUrl: '../icons/icon_128.png',
-      title: title || '',
-      message: message || '',
-    });
+    create(title, message);
   }
 
   private setupNotificationClickHandler(): void {
