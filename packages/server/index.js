a48030e2bff48f6bda6844685b81a7364296bbe6
Hugo Alliaume <kocal@live.fr>
2018-01-10 21:54:37 +0100

    Run prettier on server

--- packages/server/index.js
+++ packages/server/index.js
@@ -21,22 +21,27 @@ const parseForm = bodyParser.urlencoded({ extended: false });
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: 'solary admin',
-  store: new MemoryStore({
-    checkPeriod: 86400000,
-  }),
-  cookie: { maxAge: 60000 },
-}));
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'solary admin',
+    store: new MemoryStore({
+      checkPeriod: 86400000,
+    }),
+    cookie: { maxAge: 60000 },
+  })
+);
 app.use(flash());
 app.set('view engine', 'ejs');
 
-const httpsServer = https.createServer({
-  cert: fs.readFileSync(config.cert),
-  key: fs.readFileSync(config.key),
-}, app);
+const httpsServer = https.createServer(
+  {
+    cert: fs.readFileSync(config.cert),
+    key: fs.readFileSync(config.key),
+  },
+  app
+);
 
 // WebSocket
 const wss = new WebSocketServer({
@@ -44,13 +49,15 @@ const wss = new WebSocketServer({
   path: '/',
 });
 
-wss.on('connection', (ws) => {
-  ws.send(JSON.stringify({
-    type: 'INFO',
-    data: {
-      message: 'Connected to WebSocket server.',
-    },
-  }));
+wss.on('connection', ws => {
+  ws.send(
+    JSON.stringify({
+      type: 'INFO',
+      data: {
+        message: 'Connected to WebSocket server.',
+      },
+    })
+  );
 });
 
 // Auth
@@ -67,30 +74,47 @@ app.get('/', auth.connect(basic), csrfProtection, (req, res) => {
   });
 });
 
-app.post('/send_notification', auth.connect(basic), parseForm, csrfProtection, [
-  sanitize('notification.title').trim(),
-  sanitize('notification.message').trim(),
-  check('notification.title').exists().not().isEmpty().withMessage('Le titre de notification est vide'),
-  check('notification.message').exists().not().isEmpty().withMessage('Le message de notification est vide'),
-], (req, res) => {
-  const errors = validationResult(req);
+app.post(
+  '/send_notification',
+  auth.connect(basic),
+  parseForm,
+  csrfProtection,
+  [
+    sanitize('notification.title').trim(),
+    sanitize('notification.message').trim(),
+    check('notification.title')
+      .exists()
+      .not()
+      .isEmpty()
+      .withMessage('Le titre de notification est vide'),
+    check('notification.message')
+      .exists()
+      .not()
+      .isEmpty()
+      .withMessage('Le message de notification est vide'),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
 
-  if (errors.isEmpty()) {
-    wss.broadcast(JSON.stringify({
-      type: 'NOTIFICATION',
-      data: {
-        title: req.body.notification.title,
-        message: req.body.notification.message,
-      },
-    }));
+    if (errors.isEmpty()) {
+      wss.broadcast(
+        JSON.stringify({
+          type: 'NOTIFICATION',
+          data: {
+            title: req.body.notification.title,
+            message: req.body.notification.message,
+          },
+        })
+      );
 
-    req.flash('send_notification_success', 'La notification a bien été envoyée !');
-  } else {
-    errors.array().forEach(error => req.flash('send_notification_error', error.msg));
+      req.flash('send_notification_success', 'La notification a bien été envoyée !');
+    } else {
+      errors.array().forEach(error => req.flash('send_notification_error', error.msg));
+    }
+
+    res.redirect('/');
   }
-
-  res.redirect('/');
-});
+);
 
 app.get('/logout', auth.connect(basic), (req, res) => {
   res.status(401);
