a9189cffb062702751621e3d4051c59a55f33285
Hugo Alliaume <kocal@live.fr>
2018-02-23 15:46:18 +0100

    Add new webpack entry for options

---
+++ packages/extension/src/options/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Paramètres</h1>
+    <h2>Notifications</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem doloremque, ea fugit iure labore magni quas soluta! Harum, iusto voluptate. Incidunt nam perferendis possimus rem sit ut velit voluptatem.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style scoped>
+
+</style>
