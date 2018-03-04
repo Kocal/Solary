cfdb949723ada90133b274a3e8b9bd079c2d2f6d
Hugo Alliaume <kocal@live.fr>
2018-03-04 21:49:51 +0100

    chore(extension:test): add babel-jest

--- packages/extension/jest.config.js
+++ packages/extension/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   setupFiles: ['jest-webextension-mock', 'jest-localstorage-mock'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
