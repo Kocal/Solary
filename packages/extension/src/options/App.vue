a14f6e8df1d5bad73c16634788dcf94b369408bc
Hugo Alliaume <kocal@live.fr>
2018-03-09 14:38:52 +0100

    feat(extension): prepare options page

---
+++ packages/extension/src/options/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Notifications</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem doloremque, ea fugit iure labore magni quas soluta!</p>
+    <p class="red">Red text</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
