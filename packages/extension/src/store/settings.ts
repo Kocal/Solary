aa4015cb1b72fccda37bd2277cdbd4e6c46753ee
Hugo Alliaume <kocal@live.fr>
2018-03-09 16:12:28 +0100

    feat(extension): add settings file

---
+++ packages/extension/src/store/settings.ts
@@ -0,0 +1,35 @@
+interface Settings {
+  [k: string]: SettingsItem;
+}
+
+interface SettingsItem {
+  type: 'boolean';
+  label: string;
+  help?: string;
+  defaultValue?: any;
+  value?: any;
+  children?: Settings;
+}
+
+const settings: Settings = {
+  showNotifications: {
+    type: 'boolean',
+    label: 'Notifications',
+    defaultValue: true,
+    children: {
+      atBoot: {
+        type: 'boolean',
+        label: 'Au démarrage',
+        defaultValue: true,
+      },
+      onTitleUpdate: {
+        type: 'boolean',
+        label: 'À chaque nouvelle émission',
+        help: "Affiche une notification lorsque d'un stream change de titre.",
+        defaultValue: false,
+      },
+    },
+  },
+};
+
+export default settings;
