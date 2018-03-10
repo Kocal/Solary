f618c395f0ab09039b078a3c02cfa0a0667b4613
Hugo Alliaume <kocal@live.fr>
2018-03-10 08:33:02 +0100

    test(extension): settings manager

---
+++ packages/extension/__tests__/services/SettingsManager.test.js
@@ -0,0 +1,105 @@
+import { StorageManager } from '../../src/services/StorageManager';
+import { SettingsManager } from '../../src/services/SettingsManager';
+
+let settings;
+let storageManager;
+let settingsManager;
+
+describe('SettingsManager', () => {
+  beforeEach(() => {
+    settings = require('../../src/store/settings').default;
+    storageManager = new StorageManager();
+    settingsManager = new SettingsManager(settings, storageManager);
+
+    storageManager.get = jest.fn(new StorageManager().get);
+    storageManager.set = jest.fn(new StorageManager().set);
+  });
+
+  describe('fetch settings', () => {
+    it('should hydrate settings values', () => {
+      const syncSettings = JSON.parse(JSON.stringify(settings));
+      syncSettings.showNotifications.value = false;
+      syncSettings.showNotifications.children.atBoot.value = false;
+      syncSettings.showNotifications.children.onTitleUpdate.value = true;
+
+      storageManager.get.mockReturnValue(new Promise(resolve => resolve({ settings: syncSettings })));
+
+      // Before hydrate, it's using default values
+      expect(settingsManager.get('showNotifications')).toBeTruthy();
+      expect(settingsManager.get('showNotifications.atBoot')).toBeTruthy();
+      expect(settingsManager.get('showNotifications.onTitleUpdate')).toBeFalsy();
+
+      return settingsManager.hydrate().then(() => {
+        expect(storageManager.get).toHaveBeenCalledWith('settings');
+        expect(settingsManager.get('showNotifications')).toBeFalsy();
+        expect(settingsManager.get('showNotifications.atBoot')).toBeFalsy();
+        expect(settingsManager.get('showNotifications.onTitleUpdate')).toBeTruthy();
+      });
+    });
+  });
+
+  describe('accessors', () => {
+    test('retrieve a setting default value', () => {
+      expect(settings.showNotifications.defaultValue).toBeTruthy();
+      expect(settings.showNotifications.value).toBeUndefined();
+      expect(settingsManager.get('showNotifications')).toBeTruthy();
+      expect(storageManager.get).not.toHaveBeenCalled();
+    });
+
+    test('retrieve a child setting default value', () => {
+      expect(settings.showNotifications.children.atBoot.defaultValue).toBeTruthy();
+      expect(settings.showNotifications.children.atBoot.value).toBeUndefined();
+      expect(settingsManager.get('showNotifications.atBoot')).toBeTruthy();
+      expect(storageManager.get).not.toHaveBeenCalled();
+    });
+
+    test('retrieve a setting value', () => {
+      settings.showNotifications.value = true;
+      expect(settingsManager.get('showNotifications')).toBeTruthy();
+      expect(storageManager.get).not.toHaveBeenCalled();
+
+      settings.showNotifications.value = false;
+      expect(settingsManager.get('showNotifications')).toBeFalsy();
+      expect(storageManager.get).not.toHaveBeenCalled();
+    });
+
+    test('retrieve a child setting value', () => {
+      settings.showNotifications.children.atBoot.value = true;
+      expect(settingsManager.get('showNotifications.atBoot')).toBeTruthy();
+      expect(storageManager.get).not.toHaveBeenCalled();
+
+      settings.showNotifications.children.atBoot.value = false;
+      expect(settingsManager.get('showNotifications.atBoot')).toBeFalsy();
+      expect(storageManager.get).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('mutators', () => {
+    test('mutate setting value', () => {
+      settingsManager.set('showNotifications', false);
+      expect(storageManager.set).toHaveBeenCalledWith({ settings });
+      expect(settings.showNotifications.defaultValue).toBeTruthy();
+      expect(settings.showNotifications.value).toBeFalsy();
+
+      settingsManager.set('showNotifications', true);
+      expect(storageManager.set).toHaveBeenCalledWith({ settings });
+      expect(settings.showNotifications.defaultValue).toBeTruthy();
+      expect(settings.showNotifications.value).toBeTruthy();
+    });
+
+    test('mutate children setting value', done => {
+      settingsManager.set('showNotifications.atBoot', false).then(() => {
+        expect(storageManager.set).toHaveBeenCalledWith({ settings });
+        expect(settings.showNotifications.children.atBoot.defaultValue).toBeTruthy();
+        expect(settings.showNotifications.children.atBoot.value).toBeFalsy();
+
+        settingsManager.set('showNotifications.atBoot', true).then(() => {
+          expect(storageManager.set).toHaveBeenCalledWith({ settings });
+          expect(settings.showNotifications.children.atBoot.defaultValue).toBeTruthy();
+          expect(settings.showNotifications.children.atBoot.value).toBeTruthy();
+          done();
+        });
+      });
+    });
+  });
+});
