4c942f2df08c50fc5b3a9353788bd2b4d8bd16a9
Hugo Alliaume <kocal@live.fr>
2019-03-12 09:08:17 +0100

    use async/await (#266)

--- src/popup/router/pages/Scheduling.vue
+++ src/popup/router/pages/Scheduling.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page-scheduling">
     <a v-if="imageUrl" :href="pageUrl" target="_blank" data-testid="scheduling-url">
-      <s-image :url="imageUrl" data-testid="scheduling-image"/>
+      <s-image :url="imageUrl" data-testid="scheduling-image" />
     </a>
     <p v-if="error" class="alert alert-error">{{ error }}</p>
   </div>
@@ -26,7 +26,7 @@ export default {
     try {
       this.imageUrl = await getScheduling();
     } catch (err) {
-      this.error = error;
+      this.error = err;
     }
   },
 };
